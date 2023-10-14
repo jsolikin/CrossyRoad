093c978d107416f8665a1cfdb9a0391f0a4ff8e5
Kevin Wang <38193800+kevin1015wang@users.noreply.github.com>
2023-10-13 22:04:03 -0400

    Added heatmap

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Heatmap } from 'react-native-maps';
 
 export default function App() {
+  const data = [
+    { latitude: 37.782551, longitude: -122.445368 },
+    { latitude: 37.782745, longitude: -122.444586 },
+    { latitude: 37.782842, longitude: -122.443688 },
+    { latitude: 37.782919, longitude: -122.442815 },
+    { latitude: 37.782992, longitude: -122.442112 },
+    { latitude: 37.783100, longitude: -122.441461 },
+    { latitude: 37.783206, longitude: -122.440829 },
+    { latitude: 37.783273, longitude: -122.440324 },
+    { latitude: 37.783316, longitude: -122.440023 },
+    { latitude: 37.783357, longitude: -122.439794 },
+    { latitude: 37.783371, longitude: -122.439687 },
+    { latitude: 37.783368, longitude: -122.439666 },
+    { latitude: 37.783383, longitude: -122.439594 },
+    { latitude: 37.783508, longitude: -122.439525 },
+    { latitude: 37.783842, longitude: -122.439591 },
+    { latitude: 37.784147, longitude: -122.439668 },
+    { latitude: 37.784206, longitude: -122.439686 }
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        provider='google'
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Heatmap
+          points={data}
+          radius={40}
+          opacity={0.6}
+          gradient={{
+            colors: ["#00BFFF", "#FFA500", "#FF0000"],
+            startPoints: [0.1, 0.4, 1],
+            colorMapSize: 256
+          }}
+        />
+      </MapView>
     </View>
   );
 }
@@ -14,7 +54,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
